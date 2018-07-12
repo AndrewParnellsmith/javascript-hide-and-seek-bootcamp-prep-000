7d0e801e8ffdb4146e95685612c5b43b52269e30
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-07-12 03:35:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+   return document.querySelector(selector);
+}
 
+function nestedTarget() {
+    return document.getElementsByTagName('div')[4];
+}
+
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+        for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = ((parseInt(lis[i].innerHTML) + n));
+} 
+}
+
+function deepestChild() {
+const divs = document.querySelector('#grand-node').querySelectorAll('div')
+return divs[3]
+}
